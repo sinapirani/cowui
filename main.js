f29c8ed2594eac2019813e2a48f8633650dd3785
sina <realsinapirani@gmail.com>
2021-09-23 21:36:09 +0330

    animations added to page (i love them!)

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 let body = document
 let off = document.getElementById('off')
+let title = document.getElementById('title')
+let title2 = document.getElementById('title2')
+let titleText = ['D', 'o', 'n', "'", 't', ' ', 'B', 'e', ' ', 'L', 'a', 't', 'e', 'I', 'n', 'v', 'e', 's', 't', 'i', 'n', 'g', ' ', 'I', 's', ' ', 'G', 'r', 'e', 'a', 't', 'e']
+let text = ''
+let counter = -1
+let br = document.createElement('br')
 
-body.addEventListener('mousemove',(e)=>{
-    off.textContent = e.screenX
-})
+let titleInt = setInterval(() => {
+
+
+    counter += 1
+    if(counter <= 12){
+        title.textContent += titleText[counter]
+        console.log(counter)
+    }else if(counter >= 11 && counter < titleText.length){
+        title2.textContent += titleText[counter]
+    }
+
+},70);
